55c348c43760a6c16147534eb66ee989677b63fd
c0b20136 <c0b2013689@edu.teu.ac.jp>
2023-04-24 11:40:24 +0900

    二回目

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,5 +1,6 @@
 import pygame as pg
 import sys
+import random
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
@@ -8,6 +9,17 @@ def main():
     bg_img = pg.image.load("ex01/fig/pg_bg.jpg")
     kk_img = pg.image.load("ex01/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
+    
+    bomb_radius = 10
+    bomb_color = (255, 0, 0)
+    bomb_img = pg.Surface((bomb_radius * 2, bomb_radius * 2))
+    bomb_img.fill((0, 0, 0))
+    pg.draw.circle(bomb_img, bomb_color, (bomb_radius, bomb_radius), bomb_radius)
+    bomb_img.set_colorkey((0, 0, 0))
+    bomb_rect = bomb_img.get_rect()
+    bomb_rect.center = (random.randint(0, screen.get_width()), random.randint(0, screen.get_height()))
+    screen.blit(bomb_img, bomb_rect)
+
     tmr = 0
 
     while True:
@@ -18,6 +30,9 @@ def main():
         tmr += 1
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, [900, 400])
+
+        screen.blit(bomb_img, bomb_rect)
+
 
         pg.display.update()
         clock.tick(1000)
